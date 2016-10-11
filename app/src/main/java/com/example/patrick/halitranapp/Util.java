80704e720928d539238518f9d37100ca5c715489
tranpatrick <patra94@hotmail.fr>
2016-10-11 18:16:51 +0200

    Util file added

---
+++ app/src/main/java/com/example/patrick/halitranapp/Util.java
@@ -0,0 +1,13 @@
+package com.example.patrick.halitranapp;
+
+/**
+ * Created by Patrick on 11/10/2016.
+ */
+
+public class Util {
+
+    public static String ServerAdress = "http://132.227.201.129:8280/HALIFA_TRAN2/";
+
+    public static String INSCRIPTION = "CreateUser";
+
+}
